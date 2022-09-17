c86a392eb16716ddfa1292af8dfbcbb5a84a2a61
cssong <a1373052011@gmail.com>
2022-09-17 20:31:40 +0900

    Feat: 추가 리덕스 사가 관련 공통 함수
    comments 상태 관리 시에 필요한 리덕스 사가 공통 함수 추가

--- src/util/async.utill.js
+++ src/util/async.utill.js
@@ -1,3 +1,5 @@
+import { call, put } from "redux-saga/effects";
+
 export const reducerUtils = {
   initial: (data = null) => ({
     loading: false,
@@ -19,4 +21,97 @@ export const reducerUtils = {
     loading: false,
     error,
   }),
+};
+
+const getStatus = (type) => [`${type}_SUCCESS`, `${type}_ERROR`];
+
+export const createPromiseSaga = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = getStatus(type);
+
+  return function* saga(action) {
+    try {
+      const payload = yield call(promiseCreator, action.payload);
+      yield put({ type: SUCCESS, payload });
+    } catch (e) {
+      yield put({ type: ERROR, error: true, payload: e });
+    }
+  };
+};
+
+export const createPromiseSagaByID = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = getStatus(type);
+
+  return function* saga(action) {
+    const id = action.meta;
+
+    try {
+      const payload = yield call(promiseCreator, action.payload);
+      yield put({ type: SUCCESS, payload, meta: id });
+    } catch (e) {
+      yield put({ type: ERROR, error: true, payload: e, meta: id });
+    }
+  };
+};
+
+export const handleAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = getStatus(type);
+
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading(state[key].data),
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+};
+
+export const handleAsyncActionsById = (type, key) => {
+  const [SUCCESS, ERROR] = getStatus(type);
+
+  return (state, action) => {
+    const id = action.meta;
+
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.loading(state[key][id] && state[key][id].data),
+          },
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.success(action.payload),
+          },
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.error(action.payload),
+          },
+        };
+      default:
+        return state;
+    }
+  };
 };
