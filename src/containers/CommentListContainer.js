acb8541c7ce7d5fe287c5401d453f3dfd979fd25
cssong <a1373052011@gmail.com>
2022-09-18 15:13:05 +0900

    Feat: 추가 commentList 컨테이너에 redux 적용
    CommentList 컨테이너에 댓글을 조회하는 dispatch 적용
    api 호출하여 조회된 데이터 CommentList로 전달

--- src/containers/CommentListContainer.js
+++ src/containers/CommentListContainer.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import CommentList from "../components/CommentList";
+import { getComments } from "../modules/comments";
 
 function CommentListContainer() {
-  return <CommentList />;
+  const { data, loading, error } = useSelector((state) => state.comments);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getComments());
+  }, [dispatch]);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (data) return <CommentList comments={data} />;
 }
 
 export default CommentListContainer;
