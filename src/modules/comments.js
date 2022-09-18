f21aa9d6ef27a18c77ab3944f03725d1a8468e03
cssong <a1373052011@gmail.com>
2022-09-18 15:05:26 +0900

    Refactor: 변경 comments 모듈
    상태를 객체로 보유하려 했지만 모듈 당 하나의 상태만 적용하는 것이 관리하기 편할 것으로 판단하여 {comments} => comments로 변경
    api호출 추가
    startsWith을 사용하여 _SUCCES, _ERROR에 대한 처리

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,4 +1,4 @@
-import { call, put, takeEvery } from "redux-saga/effects";
+import { takeEvery } from "redux-saga/effects";
 import {
   createPromiseSaga,
   createPromiseSagaByID,
@@ -6,15 +6,17 @@ import {
   handleAsyncActionsById,
   reducerUtils,
 } from "../util/async.utill";
-
-const SUCCESS = "SUCCESS";
-const ERROR = "ERROR";
+import * as api from "../api/api";
 
 /* 액션 타입 */
-const GET_COMMENTS = "GET_COMMENTS";
-const SAVE_COMMENT = "SAVE_COMMENT";
-const UPDATE_COMMENT = "UPDATE_COMMENT";
-const DELETE_COMMENT = "DELETE_COMMENT";
+const actionTypeArray = [
+  "GET_COMMENTS",
+  "SAVE_COMMENT",
+  "UPDATE_COMMENT",
+  "DELETE_COMMENT",
+];
+const [GET_COMMENTS, SAVE_COMMENT, UPDATE_COMMENT, DELETE_COMMENT] =
+  actionTypeArray;
 
 /* 액션 생성 함수 */
 export const getComments = () => ({ type: GET_COMMENTS });
@@ -34,10 +36,16 @@ export const deleteComment = (id) => ({
   meta: id,
 });
 
-const getCommentsSaga = createPromiseSaga("GET_COMMENTS", () => {}); // TODO api 연동 & 통신 결과값 확인 후 수정 예정
-const saveCommentSaga = createPromiseSagaByID("SAVE_COMMENT", () => {}); // TODO api 연동 & 통신 결과값 확인 후 수정 예정
-const updateCommentSaga = createPromiseSagaByID("UPDATE_COMMNET", () => {}); // TODO api 연동 & 통신 결과값 확인 후 수정 예정
-const deleteCommentSaga = createPromiseSagaByID("DELETE_COMMENT", () => {}); // TODO api 연동 & 통신 결과값 확인 후 수정 예정
+const getCommentsSaga = createPromiseSaga(GET_COMMENTS, api.getComments);
+const saveCommentSaga = createPromiseSagaByID(SAVE_COMMENT, api.saveComment);
+const updateCommentSaga = createPromiseSagaByID(
+  UPDATE_COMMENT,
+  api.updateComment
+);
+const deleteCommentSaga = createPromiseSagaByID(
+  DELETE_COMMENT,
+  api.deleteComment
+);
 
 export function* commentsSaga() {
   yield takeEvery(GET_COMMENTS, getCommentsSaga);
@@ -49,30 +57,30 @@ export function* commentsSaga() {
 // export const goToDefaultPage = () => {}; // TODO 저장, 삭제 후 1 페이지로 이동하는 함수?
 
 /* 초기 상태 */
-const initialState = {
-  comments: reducerUtils.initial(),
-};
+const initialState = reducerUtils.initial();
 
 /* 리듀서 */
+// const reducerMap = {
+//   GET_COMMENTS: handleAsyncActions(GET_COMMENTS),
+//   SAVE_COMMENT: handleAsyncActionsById(SAVE_COMMENT),
+//   UPDATE_COMMENT: handleAsyncActionsById(UPDATE_COMMENT),
+//   DELETE_COMMENT: handleAsyncActionsById(DELETE_COMMENT),
+// };
+
 export default function comments(state = initialState, action) {
-  switch (action.type) {
-    case GET_COMMENTS:
-    case `${GET_COMMENTS}_${SUCCESS}`:
-    case `${GET_COMMENTS}_${ERROR}`:
-      return handleAsyncActions(GET_COMMENTS, "comments")(state, action);
-    case SAVE_COMMENT:
-    case `${SAVE_COMMENT}_${SUCCESS}`:
-    case `${SAVE_COMMENT}_${ERROR}`:
-      return handleAsyncActionsById(SAVE_COMMENT, "comments")(state, action);
-    case UPDATE_COMMENT:
-    case `${UPDATE_COMMENT}_${SUCCESS}`:
-    case `${UPDATE_COMMENT}_${ERROR}`:
-      return handleAsyncActionsById(UPDATE_COMMENT, "comments")(state, action);
-    case DELETE_COMMENT:
-    case `${DELETE_COMMENT}_${SUCCESS}`:
-    case `${DELETE_COMMENT}_${ERROR}`:
-      return handleAsyncActionsById(DELETE_COMMENT, "comments")(state, action);
-    default:
-      return state;
-  }
+  if (action.type.startsWith(GET_COMMENTS))
+    return handleAsyncActions(GET_COMMENTS)(state, action);
+  if (action.type.startsWith(SAVE_COMMENT))
+    return handleAsyncActionsById(SAVE_COMMENT)(state, action);
+  if (action.type.startsWith(UPDATE_COMMENT))
+    return handleAsyncActionsById(UPDATE_COMMENT)(state, action);
+  if (action.type.startsWith(DELETE_COMMENT))
+    return handleAsyncActionsById(DELETE_COMMENT)(state, action);
+
+  return state;
+  // return actionTypeArray.reduce((type) => {
+  //   return action.type.startsWith(type)
+  //     ? reducerMap[type](state, action)
+  //     : state;
+  // });
 }
