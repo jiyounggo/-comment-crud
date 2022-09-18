b31830de1cb7317c4f106678660adea1d0ddca47
cssong <a1373052011@gmail.com>
2022-09-18 17:31:52 +0900

    Feat: 삭제 createPromiseSagaByID, handleAsyncActionsById
    ___ById로 state를 관리할 이유가 없어 삭제

--- src/util/async.utill.js
+++ src/util/async.utill.js
@@ -38,59 +38,25 @@ export const createPromiseSaga = (type, promiseCreator) => {
   };
 };
 
-export const createPromiseSagaByID = (type, promiseCreator) => {
-  const [SUCCESS, ERROR] = getStatus(type);
-
-  return function* saga(action) {
-    const id = action.meta;
-
-    try {
-      const response = yield call(promiseCreator, action.payload);
-      yield put({ type: SUCCESS, payload: response.data, meta: id });
-    } catch (e) {
-      yield put({ type: ERROR, error: true, payload: e, meta: id });
-    }
-  };
-};
-
-export const handleAsyncActions = (type) => {
+export const handleAsyncActions = (type, key) => {
   const [SUCCESS, ERROR] = getStatus(type);
 
   return (state, action) => {
     switch (action.type) {
       case type:
-        return reducerUtils.loading(state.data);
-      case SUCCESS:
-        return reducerUtils.success(action.payload);
-      case ERROR:
-        return reducerUtils.error(action.payload);
-      default:
-        return state;
-    }
-  };
-};
-
-export const handleAsyncActionsById = (type) => {
-  const [SUCCESS, ERROR] = getStatus(type);
-
-  return (state, action) => {
-    const id = action.meta;
-
-    switch (action.type) {
-      case type:
         return {
           ...state,
-          [id]: reducerUtils.loading(state[id] && state[id].data),
+          [key]: reducerUtils.loading(state[key].data),
         };
       case SUCCESS:
         return {
           ...state,
-          [id]: reducerUtils.success(action.payload),
+          [key]: reducerUtils.success(action.payload),
         };
       case ERROR:
         return {
           ...state,
-          [id]: reducerUtils.error(action.payload),
+          [key]: reducerUtils.error(action.payload),
         };
       default:
         return state;
