738c5e38038aba0ff37968f6463ebfa5eabf7907
cssong <a1373052011@gmail.com>
2022-09-18 17:28:33 +0900

    Feat: 변경 api
    api 호출하는 부분 작성 완료

--- src/api/api.js
+++ src/api/api.js
@@ -6,9 +6,10 @@ const api = axios.create({
   baseURL: `${API_URL}/comments`,
 });
 
-const getComments = async () => await api.get(``);
-const saveComment = async (id) => await api.post(`/${id}`);
-const updateComment = async (id) => await api.put(`/${id}`);
+const getComments = async () => await api.get();
+const saveComment = async (comment) => api.post(``, comment);
+const updateComment = async (comment) =>
+  await api.put(`/${comment.id}`, comment);
 const deleteComment = async (id) => await api.delete(`/${id}`);
 
 export { getComments, saveComment, updateComment, deleteComment };
