5c35ac276848ad2ff8ecb5d3c9bc60c08276fa8d
cssong <a1373052011@gmail.com>
2022-09-18 17:32:56 +0900

    Feat: 변경 comment 모듈
    comment 모듈의 상태를 comments, comment로 분리

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,51 +1,36 @@
 import { takeEvery } from "redux-saga/effects";
 import {
   createPromiseSaga,
-  createPromiseSagaByID,
   handleAsyncActions,
-  handleAsyncActionsById,
   reducerUtils,
 } from "../util/async.utill";
 import * as api from "../api/api";
 
 /* 액션 타입 */
-const actionTypeArray = [
-  "GET_COMMENTS",
-  "SAVE_COMMENT",
-  "UPDATE_COMMENT",
-  "DELETE_COMMENT",
-];
-const [GET_COMMENTS, SAVE_COMMENT, UPDATE_COMMENT, DELETE_COMMENT] =
-  actionTypeArray;
+const GET_COMMENTS = "GET_COMMENTS";
+const SAVE_COMMENT = "SAVE_COMMENT";
+const UPDATE_COMMENT = "UPDATE_COMMENT";
+const DELETE_COMMENT = "DELETE_COMMENT";
 
 /* 액션 생성 함수 */
 export const getComments = () => ({ type: GET_COMMENTS });
-export const saveComment = (id) => ({
+export const saveComment = (comment) => ({
   type: SAVE_COMMENT,
-  payload: id,
-  meta: id,
+  payload: comment,
 });
-export const updateComment = (id) => ({
+export const updateComment = (comment) => ({
   type: UPDATE_COMMENT,
-  payload: id,
-  meta: id,
+  payload: comment,
 });
 export const deleteComment = (id) => ({
   type: DELETE_COMMENT,
   payload: id,
-  meta: id,
 });
 
 const getCommentsSaga = createPromiseSaga(GET_COMMENTS, api.getComments);
-const saveCommentSaga = createPromiseSagaByID(SAVE_COMMENT, api.saveComment);
-const updateCommentSaga = createPromiseSagaByID(
-  UPDATE_COMMENT,
-  api.updateComment
-);
-const deleteCommentSaga = createPromiseSagaByID(
-  DELETE_COMMENT,
-  api.deleteComment
-);
+const saveCommentSaga = createPromiseSaga(SAVE_COMMENT, api.saveComment);
+const updateCommentSaga = createPromiseSaga(UPDATE_COMMENT, api.updateComment);
+const deleteCommentSaga = createPromiseSaga(DELETE_COMMENT, api.deleteComment);
 
 export function* commentsSaga() {
   yield takeEvery(GET_COMMENTS, getCommentsSaga);
@@ -57,30 +42,21 @@ export function* commentsSaga() {
 // export const goToDefaultPage = () => {}; // TODO 저장, 삭제 후 1 페이지로 이동하는 함수?
 
 /* 초기 상태 */
-const initialState = reducerUtils.initial();
+const initialState = {
+  comments: reducerUtils.initial(),
+  comment: reducerUtils.initial(),
+};
 
 /* 리듀서 */
-// const reducerMap = {
-//   GET_COMMENTS: handleAsyncActions(GET_COMMENTS),
-//   SAVE_COMMENT: handleAsyncActionsById(SAVE_COMMENT),
-//   UPDATE_COMMENT: handleAsyncActionsById(UPDATE_COMMENT),
-//   DELETE_COMMENT: handleAsyncActionsById(DELETE_COMMENT),
-// };
-
 export default function comments(state = initialState, action) {
   if (action.type.startsWith(GET_COMMENTS))
-    return handleAsyncActions(GET_COMMENTS)(state, action);
+    return handleAsyncActions(GET_COMMENTS, "comments")(state, action);
   if (action.type.startsWith(SAVE_COMMENT))
-    return handleAsyncActionsById(SAVE_COMMENT)(state, action);
+    return handleAsyncActions(SAVE_COMMENT, "comment")(state, action);
   if (action.type.startsWith(UPDATE_COMMENT))
-    return handleAsyncActionsById(UPDATE_COMMENT)(state, action);
+    return handleAsyncActions(UPDATE_COMMENT, "comment")(state, action);
   if (action.type.startsWith(DELETE_COMMENT))
-    return handleAsyncActionsById(DELETE_COMMENT)(state, action);
+    return handleAsyncActions(DELETE_COMMENT, "comment")(state, action);
 
   return state;
-  // return actionTypeArray.reduce((type) => {
-  //   return action.type.startsWith(type)
-  //     ? reducerMap[type](state, action)
-  //     : state;
-  // });
 }
