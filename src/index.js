46522eacbfa3fe4c12afc0f769021c88f37f5c7c
cssong <a1373052011@gmail.com>
2022-09-18 15:11:36 +0900

    Feat: 추가 middelware
    redux sage를 통한 비동기 함수 추가
    미들웨어 logger 추가
    redux devlTools 적용

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import rootReducer, { rootSaga } from "./modules/rootReducer";
+import { createLogger } from "redux-logger";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware, logger))
+);
+
+sagaMiddleware.run(rootSaga);
+
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
+    </Provider>
   </BrowserRouter>
 );
 
