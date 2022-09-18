7b61e2108f230eb84420bba2b5ebfe96fcef6158
Malza0408 <parkjh04080@gmail.com>
2022-09-18 16:43:44 +0900

    Feat: 추가 commentList를 get
    commentList를 get 해 와서 CommentList에서 뿌림

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 
 const Comment = styled.div`
@@ -35,19 +36,12 @@ const Button = styled.div`
   }
 `;
 
-// 임시 데이터 입니다. 코드 작성시 data 부분을 지워주세요
-const data = [
-  {
-    id: 1,
-    profile_url: "https://picsum.photos/id/1/50/50",
-    author: "abc_1",
-    content: "UI 테스트는 어떻게 진행하나요",
-    createdAt: "2020-05-01",
-  },
-];
+function CommentList({ comments, getComments }) {
+  useEffect(() => {
+    getComments();
+  }, [getComments]);
 
-function CommentList() {
-  return data.map((comment, key) => (
+  return comments.map((comment, key) => (
     <Comment key={key}>
       <img src={comment.profile_url} alt="" />
 
--- src/containers/CommentListContainer.js
+++ src/containers/CommentListContainer.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import CommentList from "../components/CommentList";
+import { getCommentsThunk } from "../redux/modules/comments";
 
 function CommentListContainer() {
-  return <CommentList />;
+  const comments = useSelector((state) => state.comments.data);
+  const dispatch = useDispatch();
+
+  const getComments = useCallback(() => {
+    dispatch(getCommentsThunk());
+  }, [dispatch]);
+
+  return <CommentList comments={comments} getComments={getComments} />;
 }
 
 export default CommentListContainer;
