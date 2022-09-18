3a77040eac055fd21994094632831ced8854bb7e
Sujin Choi <hissujinc@gmail.com>
2022-09-18 18:26:24 +0900

    Refactor: 수정 redux 진행중

--- src/containers/CommentListContainer.js
+++ src/containers/CommentListContainer.js
@@ -1,7 +1,17 @@
-import React from "react";
-import CommentList from "../components/CommentList";
+import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import CommentList from '../components/CommentList';
+import { getComments } from '../modules/comments';
 
 function CommentListContainer() {
+  const { comments, comment } = useSelector((state) => state.comments);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getComments());
+    console.log(comments);
+  });
   return <CommentList />;
 }
 
--- src/index.js
+++ src/index.js
@@ -2,46 +2,26 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer, { rootSaga } from './modules';
-import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import rootReducer, { rootSaga } from './modules';
 
 const container = document.getElementById('root');
-
-const customHistory = createBrowserHistory();
-const sagaMiddleware = createSagaMiddleware({
-  context: {
-    history: customHistory,
-  },
-}); // 사가 미들웨어를 만듭니다.
-
-const store = createStore(
-  rootReducer,
-  // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
-  composeWithDevTools(
-    applyMiddleware(
-      sagaMiddleware, // 사가 미들웨어를 적용하고
-      logger
-    )
-  )
-); // 여러개의 미들웨어를 적용 할 수 있습니다.
-
-sagaMiddleware.run(rootSaga); // 루트 사가를 실행해줍니다.
-
 const root = createRoot(container);
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware, logger)));
+
+sagaMiddleware.run(rootSaga);
 
 root.render(
   <BrowserRouter>
-    {/* <Router history={customHistory}> */}
     <Provider store={store}>
       <App />
     </Provider>
-    {/* </Router> */}
   </BrowserRouter>
 );
